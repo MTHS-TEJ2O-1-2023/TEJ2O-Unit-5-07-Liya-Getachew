c5142af5472438b6cabb810ac43470d6800d57b8
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2023-11-08 12:57:06 -0500

    ts vers

--- main.ts
+++ main.ts
@@ -5,4 +5,32 @@
  * This program moves the servo back and forth.
 */
 
-basic.showString('Hello, World!')
+// variables
+const servoNumber1 = robotbit.Servos.S1
+
+// setup
+basic.showIcon(IconNames.QuarterNote)
+
+// turns servo to 0 degrees
+input.onButtonPressed(Button.A, function () {
+  robotbit.Servo(servoNumber1, 0)
+  basic.clearScreen()
+  basic.showString('0')
+  basic.showIcon(IconNames.QuarterNote)
+
+  // when servo finishes turning
+  basic.clearScreen()
+  basic.showIcon(IconNames.QuarterNote)
+})
+
+// turns servo to 180 degrees
+input.onButtonPressed(Button.B, function () {
+  robotbit.Servo(servoNumber1, 180)
+  basic.clearScreen()
+  basic.showString('180')
+  basic.showIcon(IconNames.SmallSquare)
+
+  // when servo finishes turning
+  basic.clearScreen()
+  basic.showIcon(IconNames.EigthNote)
+})
