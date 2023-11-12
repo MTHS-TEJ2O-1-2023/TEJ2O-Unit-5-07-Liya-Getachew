9ecf810ecd649bebc7bad92817234c9dc5ce13f3
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2023-11-12 18:10:53 +0000

    flow

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,13 +1,37 @@
 """
-Created by: liya 
+Created by: Liya G
 Created on: Nov 2023
 This module is a Micro:bit MicroPython program moves the servo back and forth.
 """
 
 from microbit import *
-import robotbit 
 
 
 # variables
+servo = 0
 
+# set up
+display.clear()
+display.show(Image.HOUSE)
 
+# loop
+while True:
+    if button_a.is_pressed():
+        servo = pin16.set_analog_period(1)
+        display.clear()
+        display.scroll("0")
+        display.show(Image.SMILE)
+
+        # when motor finishes turn
+        display.clear()
+        display.show(Image.SMILE)
+
+    if button_b.is_pressed():
+        pin16.set_analog_period(2)
+        display.clear()
+        display.scroll("0")
+        display.show(Image.SMILE)
+
+        # when motor finishes turn
+        display.clear()
+        display.show(Image.SMILE)
